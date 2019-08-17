bb159202ddb245045738dba76c6fea36ff620495
Matthew Taylor <mtaylor2096@gmail.com>
2019-08-17 16:46:26 -0400

    Bootstrap properly imported into repository

--- matthewstaylor-dev/src/app/app.component.ts
+++ matthewstaylor-dev/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+})
+
+@NgModule ({
+  imports: [
+    NgbModule,
+  ],
 })
 export class AppComponent {
   title = 'matthewstaylor-dev';
